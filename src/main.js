9f430dcc8a4c0cc6c3116b7fd6d6742183125170
LordSwaglington <LordSwaglington>
2020-03-10 21:47:06 +0100

    set up bot connection

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
 'use strict';
 
-const Discord = require('discord.js');
-const cache = require('./lib/cache');
+require('dotenv').config();
 
-console.log('it just works');
+const Discord = require('discord.js');
+const fs = require('fs');
+
+const client = new Discord.Client();
+const token = process.env.TOKEN;
+const PREFIX = 'senpai';
+
+// load command list
+client.commands = new Discord.Collection();
+const cmdFiles = fs
+    .readdirSync('./src/commands/')
+    .filter(file => file.endsWith('.js'));
+for (const file of cmdFiles) {
+    const command = require(`./src/commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+// log in
+client.login(token);
+
+// on client online
+client.on('ready', () => {
+    console.log('MemeChan is online');
+    client.user.setActivity('with reddit-chan');
+});
